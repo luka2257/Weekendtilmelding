d61c9de09910a0646a0ae3f5e4f738f1e465686a
Lukas Lykke Jensen <lukaslykkejensen1@gmail.com>
2024-10-25 16:39:11 +0200

    feat: Create Weekendtilmelding object in form action

--- src/lib/server/tilmelding.ts
+++ src/lib/server/tilmelding.ts
@@ -1,3 +1,5 @@
+import type { Weekendtilmelding } from "$lib/types";
+
 export function validateSubmission(data: FormData): boolean {
     const navn = data.get("navn");
     const værelse = data.get("værelse");
@@ -9,29 +11,31 @@ export function validateSubmission(data: FormData): boolean {
 }
 
 export function createWeekendtilmelding(data: FormData) {
-    // const tilmelding: Partial<Weekendtilmelding> & Pick<Weekendtilmelding, "navn" | "værelse" | "erTilstede"> = {
-    //   navn: data.get("navn") ? String(data.get("navn")) : "",
-    //   værelse: data.get("værelse") ? String(data.get("værelse")) : "",
-    //   erTilstede: Boolean(data.get("erTilstede")),
-    //   meals: {
-    //     FreAf: boolean,
-    //     FreKa: boolean,
-    //     LørMo: boolean,
-    //     LørMi: boolean,
-    //     LørEf: boolean,
-    //     LørAf: boolean,
-    //     LørKa: boolean,
-    //     SønMo: boolean,
-    //     SønMi: boolean,
-    //     SønEf: boolean,
-    //     SønAf: boolean,
-    //     SønKa: boolean,
-    //   },
-    //   kommentar: string,
-    //   gæst: {
-    //     navn: string,
-    //     værelse: string,
-    //     besked: string,
-    //   },
-    // };
+    const tilmelding: Weekendtilmelding = {
+      navn: String(data.get("navn")),
+      værelse: String(data.get("værelse")),
+      erTilstede: Boolean(data.get("erTilstede")),
+      meals: {
+        FreAf: Boolean(data.get("FreAf")),
+        FreKa: Boolean(data.get("FreKa")),
+        LørMo: Boolean(data.get("LørMo")),
+        LørMi: Boolean(data.get("LørMi")),
+        LørEf: Boolean(data.get("LørEf")),
+        LørAf: Boolean(data.get("LørAf")),
+        LørKa: Boolean(data.get("LørKa")),
+        SønMo: Boolean(data.get("SønMo")),
+        SønMi: Boolean(data.get("SønMi")),
+        SønEf: Boolean(data.get("SønEf")),
+        SønAf: Boolean(data.get("SønAf")),
+        SønKa: Boolean(data.get("SønKa")),
+      },
+      kommentar: String(data.get("comment")),
+      gæst: {
+        navn: String(data.get("guestName")),
+        værelse: String(data.get("guestRoom")),
+        besked: String(data.get("guestMessage")),
+      },
+    };
+    console.log("Registreret tilmelding:")
+    console.log(tilmelding);
 }
