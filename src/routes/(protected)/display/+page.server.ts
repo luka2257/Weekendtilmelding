759defb7fbf28b3fd55df3e07e81a1c6b374f4ae
Lukas Lykke Jensen <lukaslykkejensen1@gmail.com>
2024-11-18 13:55:02 +0100

    fix: :bug: Logout button on display site fix

--- src/routes/(protected)/display/+page.server.ts
+++ src/routes/(protected)/display/+page.server.ts
@@ -1,5 +1,7 @@
 import { getAllUsers } from "$lib/server/bruger";
 import { getAllTilmeldinger } from "$lib/server/db/db";
+import { loggedInUser } from "$lib/server/stores";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "../../$types";
 
 export const load: PageServerLoad = ({ params }) => {
@@ -8,3 +10,10 @@ export const load: PageServerLoad = ({ params }) => {
         tilmeldinger: getAllTilmeldinger()
 	};
 };
+
+export const actions = {
+	logout: async({ request }) => {
+	  loggedInUser.set(null);
+	  throw redirect(303, '/');
+	}
+  };
